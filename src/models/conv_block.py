42228f0a88fa5e67ebc8c5307936596456d433e9
plutasnyy <kamil.plucinski97@gmail.com>
2021-06-14 14:22:55 +0200

    revert relu

--- src/models/conv_block.py
+++ src/models/conv_block.py
@@ -8,7 +8,7 @@ class ConvolutionalBlock(nn.Module):
         self.conv1 = nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride,
                                padding_mode=padding_mode)
         self.bn1 = nn.BatchNorm1d(out_channels)
-        self.relu1 = nn.Sigmoid()
+        self.relu1 = nn.ReLU()
 
     def forward(self, x):
         out = self.conv1(x)
